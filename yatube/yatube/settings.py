39a2c55085418956361c969b20a3a5c47a7646bb
DenisSivko <densivko2306@yandex.ru>
2021-05-09 13:06:39 +0300

    adding debug_toolbar + ngrok

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = 'q8)8xfka1kk8utv#*t%wox+jdlz#=qgwd3a40hs2^!0mn5kd#y'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "[::1]",
-    "testserver",
+    "*",
 ]
 
 
@@ -46,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -154,3 +153,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
